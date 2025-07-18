e77886f0e255c8924dfdc40c88883b867fc5d366
Oliwier Rejniak <oliwier.rejniak@gmail.com>
2025-07-18 18:31:08 +0200

    Successfull install of tsParticles package and use it local instead of CDN (caused by page loading delays)

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
-module.exports = function(eleventyConfig) {
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+export default function (eleventyConfig) {
   
-  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+eleventyConfig.addPassthroughCopy({ "src/js": "js" });
 eleventyConfig.addPassthroughCopy({ "src/css": "css" });
 eleventyConfig.addPassthroughCopy({ "src/media": "media" });
+eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      root: ".11ty-vite",
+      build: {
+        outDir: "../_site",
+        emptyOutDir: true,
+      },
+      server: {
+        port: 8080,
+        strictPort: true,
+        watch: {
+          usePolling: true,
+        },
+      },
+    }
+  });
 
   // Ustawiamy foldery
   return {
